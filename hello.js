d2959f70d28f5fe7706aacf14f3a2986e0f9165c
seois830926 <idmahoro@gmail.com>
2018-12-14 11:43:17 +0900

    add console func

--- hello.js
+++ hello.js
@@ -1,4 +1,10 @@
 function test (prop, value) {
 	this.prop = prop;
 	this.value = value;	
+
+	console.log(this.prop, this.value);
+}
+
+test.prototype.console = function(arr) {
+	console.log(arr[0]);
 }
